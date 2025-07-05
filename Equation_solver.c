47a49a39d0c749571885a56899bf1a75623b0a83
Hamdi Emad <hamdyemadelgohary2004@gmail.com>
2025-07-05 23:40:28 +0300

    Updating Equation_solver source file

--- Equation_solver.c
+++ Equation_solver.c
@@ -4,34 +4,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define pi 3.14159
-float det_2(float co[2][2]); // To calculate 2x2 determinant
-float det_3(float co[3][3]); // To calculate 3x3 determinant
-float det_4(float co[4][4]); // To calculate 4x4 determinant
+double det(int k, double co[k][k]); // to calculate determinants
 
 void slv_1stdegpoly(); // To solve linear equation
 void slv_2nddegpoly(); // To solve quadratic equation(a*x^2 +b*x +c = 0)
 void slv_3rddegpoly(); // To solve cubic equation(a*x^3 +b*x^2 +cx +d = 0)
 
-void inputpoly(const unsigned short k, float *coeff);
+void inputpoly(int k, float *coeff);
 /*-It receives the coefficients from the user and stores it in an array
  -Its parameters are the degree of the polynomial and the coefficients array*/
 
-void printpolsym(float coeff[], const unsigned short k);
+void printpolsym(float coeff[], int k);
 /*-To print the polynomial equation in a symbolic way
 -Its parameters are the degree of the polynomial and the coefficients array*/
+void solve_lineareqns(int k); // solving system of linear equations using Cramer's method
 
-void slv_2lineareqns();
-// To solve a system of 2 linear equations(an*X1 +bn*X2 = cn) Using Cramer's method
-void slv_3lineareqns();
-// To solve a system of 3 linear equations(an*X1 +bn*X2 +cn*X3 = dn) Using Cramer's method
-void slv_4lineareqns();
-// To solve a system of 4 linear equations(an*X1 +bn*X2 +cn*X3 +dn*X4 = en) Using Cramer's method
-
-void inputsys(const unsigned short k, float coeff[k][k + 1]);
+void inputsys(int k, double coeff[k][k + 1]);
 /*-It receives the coefficients from the user and stores it in an array
 -Its parameters are the order of the system and the coefficients array*/
 
-void printsys(const unsigned short k, float coeff[k][k + 1]);
+void printsys(int k, double coeff[k][k + 1]);
 /*-To print the system of linear equations in a symbolic way
   -Its parameters are the order of the system and the coefficients array*/
 
@@ -43,32 +35,26 @@ int main(void)
         printf("1- Linear equation(a*X +b = 0)\n");
         printf("2- Quadratic equation(a*x^2 +b*x +c = 0)\n");
         printf("3- Cubic equation(a*x^3 +b*x^2 +cx +d = 0)\n");
-        printf("4- System of 2 linear equations(an*X1 +bn*X2 = cn)\n");
-        printf("5- System of 3 linear equations(an*X1 +bn*X2 +cn*ْX3 = dn)\n");
-        printf("6- System of 4 linear equations(an*X1 +bn*X2 +cn*ْX3 +dn*X4 = en)\n");
+        printf("4- System of n linear equations(an*X1 +bn*X2 +..... = c)\n");
         printf("Press the corresponding number to choose the problem type\n\n");
-        char c = 0, t = 0;
+        char c = 0, t = 0, k;
         t = getch();
         switch (t)
         {
-            case ('1'):
-                slv_1stdegpoly();
-                break;
-            case ('2'):
-                slv_2nddegpoly();
-                break;
-            case ('3'):
-                slv_3rddegpoly();
-                break;
-            case ('4'):
-                slv_2lineareqns();
-                break;
-            case ('5'):
-                slv_3lineareqns();
-                break;
-            case ('6'):
-                slv_4lineareqns();
-                break;
+        case ('1'):
+            slv_1stdegpoly();
+            break;
+        case ('2'):
+            slv_2nddegpoly();
+            break;
+        case ('3'):
+            slv_3rddegpoly();
+            break;
+        case ('4'):
+            printf("Please enter the number of equations you want to solve: ");
+            scanf("%d", &k);
+            solve_lineareqns(k);
+            break;
         }
         printf("Press any key to exit, Y to continue\n\n ");
         // Asks the user whether to continue or exit
@@ -175,7 +161,7 @@ void slv_3rddegpoly() // To solve qubic equation(a*x^3 +b*x^2 +cx +d = 0)
         }
     }
 }
-void inputpoly(const unsigned short k, float *coeff)
+void inputpoly(int k, float *coeff)
 /*-It receives the coefficients from the user and stores it in an array
   -Its parameters are the degree of the polynomial and the coefficients array*/
 {
@@ -199,7 +185,7 @@ void inputpoly(const unsigned short k, float *coeff)
     free(coeffo);
 }
 
-void printpolsym(float coeff[], const unsigned short k)
+void printpolsym(float coeff[], int k)
 /* -To print the polynomial equation in a symbolic way
 -Its parameters are the degree of the polynomial and the coefficients array*/
 {
@@ -221,124 +207,66 @@ void printpolsym(float coeff[], const unsigned short k)
     }
     printf(" = 0 are\n");
 }
+double det(int k, double co[k][k]) // To calculate determinants
+{
+    double submat[k][k - 1][k - 1], det1 = 0.0;
+    if (k == 1)
+        return co[0][0];
+    else if (k == 2)
+        return (co[0][0] * co[1][1]) - (co[0][1] * co[1][0]);
 
-float det_2(float co[2][2]) // To calculate 2x2 determinant
-{
-    float det = (co[0][0] * co[1][1]) - (co[0][1] * co[1][0]);
-    return det;
-}
-float det_3(float co[3][3]) // To calculate 3x3 determinant
-{
-    float submat1[2][2] = {{co[1][1], co[1][2]}, {co[2][1], co[2][2]}};
-    float submat2[2][2] = {{co[1][0], co[1][2]}, {co[2][0], co[2][2]}};
-    float submat3[2][2] = {{co[1][0], co[1][1]}, {co[2][0], co[2][1]}};
-    float det = co[0][0] * det_2(submat1) - co[0][1] * det_2(submat2) + co[0][2] * det_2(submat3);
-    return det;
-}
-float det_4(float co[4][4]) // To calculate 4x4 determinant
-{
-    float submat1[3][3] = {{co[1][1], co[1][2], co[1][3]}, {co[2][1], co[2][2], co[2][3]}, {co[3][1], co[3][2], co[3][3]}};
-    float submat2[3][3] = {{co[1][0], co[1][2], co[1][3]}, {co[2][0], co[2][2], co[2][3]}, {co[3][0], co[3][2], co[3][3]}};
-    float submat3[3][3] = {{co[1][0], co[1][1], co[1][3]}, {co[2][0], co[2][1], co[2][3]}, {co[3][0], co[3][1], co[3][3]}};
-    float submat4[3][3] = {{co[1][0], co[1][1], co[1][2]}, {co[2][0], co[2][1], co[2][2]}, {co[3][0], co[3][1], co[3][2]}};
-    float det = co[0][0] * det_3(submat1) - co[0][1] * det_3(submat2) + co[0][2] * det_3(submat3) - co[0][3] * det_3(submat4);
-    return det;
-}
-
-void slv_2lineareqns() // To solve a system of 2 linear equations(an*X1 +bn*X2 = cn) Using Cramer's method
-{
-    float D[2][2] = {0}, D1[2][2] = {0}, D2[2][2] = {0}, coeff[2][3] = {0}, x1 = 0, x2 = 0;
-    printf("Enter the coefficients of the system of equations(an*X1 +bn*X2 = cn): \n");
-    inputsys(2, coeff);
-    for (int i = 0; i < 2; ++i)
+    for (int i = 0; i < k; ++i)
     {
-        for (int j = 0; j < 2; ++j)
+        for (int j = 0; j < k - 1; ++j)
         {
-            D[i][j] = coeff[i][j];
-            D1[i][j] = coeff[i][2 - j];
-            D2[i][j] = coeff[i][j == 0 ? 0 : 2];
+            for (int t = 0, y = 0; y < k - 1; ++t, ++y)
+            {
+                if (t == i)
+                    ++t;
+
+                submat[i][j][y] = co[j + 1][t];
+            }
+        }
+        det1 += pow(-1, i) * co[0][i] * det(k - 1, submat[i]);
+    }
+
+    k--;
+    return det1;
+}
+
+void solve_lineareqns(int k) // solving system of linear equations using Cramer's method
+
+{
+    printf("Enter the coefficients of the system of %d linear equations: \n", k);
+    double Dn[k][k][k], coeff[k][k + 1], D[k][k], R[k];
+    inputsys(k, coeff);
+    for (int i = 0; i < k; ++i)
+    {
+        for (int j = 0; j < k; ++j)
+        {
+            for (int t = 0; t < k; ++t)
+            {
+                D[j][t] = coeff[j][t];
+                Dn[i][j][t] = coeff[j][t == i ? k : t];
+                R[i] = det(k, Dn[i]) / det(k, D);
+            }
         }
     }
-    x1 = det_2(D1) / det_2(D);
-    x2 = det_2(D2) / det_2(D);
-    printsys(2, coeff);
 
-    if (det_2(D) == 0 || det_2(D) == NAN || fabs(x1) == NAN || fabs(x2) == NAN || fabs(x1) == INFINITY || fabs(x2) == INFINITY)
-        printf("has infinite solutions or no solution\n");
-    else if (x1 == 0 && x2 == 0)
-        printf("only has solution of this system is ZERO solution which is X1 = X2 = 0\n");
+    printsys(k, coeff);
+    if (det(k, D) == 0 || det(k, D) == NAN)
+        printf("\nhas infinite solutions or no solution\n");
     else
     {
-
-        printf("has solutions: \nX1 = %.3f\tX2 = %.3f\n", x1, x2);
-    }
-}
-void slv_3lineareqns() // To solve a system of 3 linear equations(an*X1 +bn*X2 +cn*ْX3 = dn) Using Cramer's method
-{
-    float D[3][3] = {0}, D1[3][3] = {0}, D2[3][3] = {0}, D3[3][3] = {0}, coeff[3][4] = {0}, x1 = 0, x2 = 0, x3 = 0, x4 = 0;
-    printf("Enter the coefficients of the system of equations(an*X1 +bn*X2 +cn*X3 = dn): \n");
-    inputsys(3, coeff);
-    for (int i = 0; i < 3; ++i)
-    {
-        for (int j = 0; j < 3; ++j)
+        printf("\nhas solutions: ");
+        for (int i = 0; i < k; ++i)
         {
-            D[i][j] = coeff[i][j];
-            D1[i][j] = coeff[i][j == 0 ? 3 : j];
-            D2[i][j] = coeff[i][j == 1 ? 3 : j];
-            D3[i][j] = coeff[i][j == 2 ? 3 : j];
+            printf("\nX%d = %-+15.3lf", i + 1, R[i]);
         }
-    }
-    x1 = det_3(D1) / det_3(D);
-    x2 = det_3(D2) / det_3(D);
-    x3 = det_3(D3) / det_3(D);
-    printsys(3, coeff);
-
-    if (det_3(D) == 0 || det_3(D) == NAN || fabs(x1) == NAN || fabs(x2) == NAN || fabs(x3) == NAN || fabs(x1) == INFINITY ||
-        fabs(x2) == INFINITY || fabs(x3) == INFINITY)
-        printf("has infinite solutions or no solution\n");
-    else if (x1 == 0 && x2 == 0 && x3 == 0)
-        printf("only has solution is ZERO solution which is X1 = X2 = X3 = 0\n");
-    else
-    {
-
-        printf("has solutions: \nX1 = %.3f\tX2= %.3f\tX3 = %.3f\n", x1, x2, x3);
+        printf("\n");
     }
 }
-
-void slv_4lineareqns() // To solve a system of 4 linear equations(an*X1 +bn*X2 +cn*ْX3 +dn*X4 = en) Using Cramer's method
-{
-    float D[4][4], D1[4][4], D2[4][4], D3[4][4], D4[4][4], coeff[4][5] = {0}, x1, x2, x3, x4;
-    printf("Enter the coefficients of the system of equations(an*X1 +bn*X2 +cn*X3 +dn*X4 = en): \n");
-    inputsys(4, coeff);
-
-    for (int i = 0; i < 4; ++i)
-    {
-        for (int j = 0; j < 4; ++j)
-        {
-            D[i][j] = coeff[i][j];
-            D1[i][j] = coeff[i][j == 0 ? 4 : j];
-            D2[i][j] = coeff[i][j == 1 ? 4 : j];
-            D3[i][j] = coeff[i][j == 2 ? 4 : j];
-            D4[i][j] = coeff[i][j == 3 ? 4 : j];
-        }
-    }
-    x1 = det_4(D1) / det_4(D);
-    x2 = det_4(D2) / det_4(D);
-    x3 = det_4(D3) / det_4(D);
-    x4 = det_4(D4) / det_4(D);
-    printsys(4, coeff);
-    if ((det_4(D) == 0 || det_4(D) == NAN) || x1 == NAN || x2 == NAN || x3 == NAN || x4 == NAN || x1 == INFINITY ||
-        x2 == INFINITY || x3 == INFINITY || x4 == INFINITY)
-        printf("has infinite solutions or no solution\n");
-    else if (x1 == 0 && x2 == 0 && x3 == 0 && x4 == 0)
-        printf("only has solution is ZERO solution which is X1 = X2 = X3 = X4 = 0\n");
-    else
-    {
-
-        printf("has solutions: \nX1 = %.3f \tX2 = %.3f \tX3 = %.3f \tX4 = %.3f \n", x1, x2, x3, x4);
-    }
-}
-void inputsys(const unsigned short k, float coeff[k][k + 1])
+void inputsys(int k, double coeff[k][k + 1])
 /*-It receives the coefficients from the user and stores it in an array
         -Its parameters are the order of the system and the coefficients array*/
 {
@@ -349,7 +277,7 @@ void inputsys(const unsigned short k, float coeff[k][k + 1])
     {
         for (int j = 0; j < k + 1; ++j)
         {
-            float *r = &coeff[i][j];
+            double *r = &coeff[i][j];
 
             printf("%c%d = ", 'a' + j, i + 1);
             scanf("%s", coeffo);
@@ -366,7 +294,7 @@ void inputsys(const unsigned short k, float coeff[k][k + 1])
     free(coeffo);
 }
 
-void printsys(const unsigned short k, float coeff[k][k + 1]) /*-To print the system of linear equations in a symbolic way
+void printsys(int k, double coeff[k][k + 1]) /*-To print the system of linear equations in a symbolic way
                                                                -Its parameters are the order of the system and the coefficients array*/
 {
     printf("\nThe system:\n");
